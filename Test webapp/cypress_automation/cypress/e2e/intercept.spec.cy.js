7516e70c4c52b252044e2715fcba761447dc83a2
Kliment Miroslav <Miroslav.Kliment@airbank.cz>
2022-10-24 23:35:56 +0200

    commiting the intercept test

---
+++ Test webapp/cypress_automation/cypress/e2e/intercept.spec.cy.js
@@ -0,0 +1,25 @@
+const { expect } = require("chai")
+
+describe('mocking with intercept', ()=>{
+   
+    it('mocking with intercept test', ()=>{
+
+        cy.visit('https://rahulshettyacademy.com/angularAppdemo/');
+        
+        cy.intercept({
+            method: 'GET',
+            url: 'https://rahulshettyacademy.com/Library/GetBook.php?AuthorName=shetty'
+        },
+        {
+            statusCode: 200,
+            body: [{
+                    "book_name": "RobotFramework",
+                    "isbn": "984353",
+                    "aisle": "982053"
+                }]
+        }).as('bookretrievals')
+        cy.get("button[class='btn btn-primary']").click()
+        cy.wait('@bookretrievals')
+
+    })
+})
